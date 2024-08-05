7db800523b8058152026c28263144d57944212f6
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2024-08-05 12:12:26 +0530

    New Projects

--- 25_day/main.py
+++ 25_day/main.py
@@ -1,7 +1,18 @@
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(type(data))
-temp_list = data["temp"].to_list()
 
-print(data["list"].mean())
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20240804.csv")
+
+gray_squi = len(data[data["Primary Fur Color"] == "Gray"])
+
+black_squi = len(data[data["Primary Fur Color"] == "Black"])
+
+red_squi = len(data[data["Primary Fur Color"] == "Cinnamon"])
+
+data_dict = {
+    "fur color" : ["gray", "red", "black"],
+    "count": [gray_squi, red_squi, black_squi]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("New dataframe")
