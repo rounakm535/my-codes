c5adee647e4069ef9a4e6d31c2963d7c54effd3a
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2024-10-25 14:22:11 +0530

    comment update

--- amazon price tracker/main.py
+++ amazon price tracker/main.py
@@ -5,7 +5,7 @@ import os
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+# Load environment variables from .env filelfkdsd;afldksh
 load_dotenv()
 
 MY_EMAIL = "rounakmishra1508@gmail.com"
